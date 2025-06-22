863d46fb19c34accc28d76a22844a650d1421a6c
yascibre <yascibre@gmail.com>
2025-06-22 16:04:10 -0300

    Ajustando tamanho da tela balanco_window.py

--- scripts/balanco_window.py
+++ scripts/balanco_window.py
@@ -154,6 +154,6 @@ class BalancoWindow(QDialog):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = BalancoWindow()
-    window.resize(100, 600)
+    window.resize(1200, 600)
     window.show()
     sys.exit(app.exec())
